26c6fee05f288bd3e92ee08e5ce44bceb2def637
Bettye <155570293+Bjbrono@users.noreply.github.com>
2024-07-19 05:59:41 -0500

    Create sudoanimals.py

---
+++ sudoanimals.py
@@ -0,0 +1,11 @@
+# CODE MADE BY BETTYE TAYLOR 
+# TIME TO COMPLETE PROJECT: 20-35 Minutes 
+
+# Simple Math Program in python 
+# Change thr values to get differnt numbers 
+cat = 20 + 20
+dog = 30/20
+fish = 30/cat
+print(cat-fish+dog)
+# Cat, dog, and fish are values that can be chnaged in this program You can add more animals to this list to come up with differnt outcomes. 
+# These values are know as 'constants' and are often set values used  for non-changeable values for the user. However we can change these values.
